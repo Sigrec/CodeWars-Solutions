17be83eed328bda7aac9db4a407ae6f66bf86959
Sean Njenga <53946709+Sigrec@users.noreply.github.com>
2021-01-05 16:42:50 -0600

    Update JavaSolution.java

--- Rectangle into Squares/JavaSolution.java
+++ Rectangle into Squares/JavaSolution.java
@@ -3,25 +3,25 @@ import java.util.List;
 
 public class SqInRect {
 	public static List<Integer> sqInRect(int lng, int wdth) {
-	  if(lng == wdth){
-      return null;
-    }
-    else{
-      int recSize = lng * wdth;
-      int min = Math.min(lng, wdth);
-      int max = Math.max(lng, wdth);
-      int temp, count;
-      List<Integer> output = new ArrayList<Integer>();
-      while(recSize != 0){
-        count = max / min;
-        for (int x = 0; x < count; x++){
-          output.add(min);
-        }
-        recSize = recSize - (min * min * count);
-        max = min;
-        min = recSize / min;
-      }
-      return output;
-    }
+		if(lng == wdth){
+			return null;
+    		}
+    		else{
+		      int recSize = lng * wdth;
+		      int min = Math.min(lng, wdth);
+		      int max = Math.max(lng, wdth);
+		      int temp, count;
+		      List<Integer> output = new ArrayList<Integer>();
+		      while(recSize != 0){
+			count = max / min;
+			for (int x = 0; x < count; x++){
+			  output.add(min);
+			}
+			recSize = recSize - (min * min * count);
+			max = min;
+			min = recSize / min;
+		      }
+		      return output;
+    		}
 	}
 }
